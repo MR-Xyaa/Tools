4dab1aaa80ad962ee34b6a2b0ac5b2f8fa1dcebb
IBNU UMAR SYAID <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-02 18:11:56 +0700

    Update tools.py

--- module/tools.py
+++ module/tools.py
@@ -9,30 +9,34 @@ O = '\x1b[1;96m' # BIRU MUDA
 N = '\x1b[0m'    # WARNA MATI
 ##################
 
-
+def nama():
+      os.system('clear')
+      user_reply = input("Siapa Namamu? \n")
+nama()
 import random
 import sys
 import time
+import os
 def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(random.random() * 0.1)
-user_reply = input("Siapa Namamu? \n")
 
 
-print("Pengguna:", user_reply)
-mengetik('===============[ MENU ]===============[+]')
-mengetik('\x1b[1;94m[1]___Install Bahan')
+
+mengetik('\x1b[1;92m===============[ MENU ]===============[+]')
+mengetik('\x1b[1;91m[1]___Install Bahan')
 mengetik('\x1b[1;93m[2]___Tampilan Termux Oh-My-Zsh')
-mengetik('[3]___MTK')
-mengetik('[4]___DMBF')
-mengetik('[5]___KangSpam')
-mengetik('[6]___Terkey By MR-Xyaa')
-mengetik('[7]___Spamcall By MR-Xyaa')
-mengetik('[8]___Spam By Bang Xenzi')
-mengetik('[0]__EXIT MENU MR-Xyaa')
-mengetik('[+]===============[ MENU ]===============[+]')
-mengetik('Silahkan Pilih Menunya Bang')
+mengetik('\x1b[1;94m[3]___MTK')
+mengetik('\x1b[1;95m[4]___DMBF')
+mengetik('\x1b[1;96m[5]___KangSpam')
+mengetik('\x1b[1;97m[6]___Terkey By MR-Xyaa')
+mengetik('\x1b[1;98m[7]___Spamcall By MR-Xyaa')
+mengetik('\x1b[1;91m[8]___Spam By Bang Xenzi')
+mengetik('\x1b[1;93m[0]__EXIT MENU MR-Xyaa')
+mengetik('\x1b[1;92m[+]===============[ MENU ]===============[+]')
+print("Pengguna:", user_reply)
+mengetik('Silahkan Pilih Menunya')
 
 
