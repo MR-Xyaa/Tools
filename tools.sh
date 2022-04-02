07439cad50580e06ebec18f4340c585be019c77d
IBNU UMAR SYAID <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-02 18:40:08 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -26,9 +26,149 @@ sleep 1
 
 read -p "MR-Xyaa Pilih No:" bro
 
+## MENU1
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
 toilet -f slant "MR-Xyaa";
-pkg update
+pkg update && pkg upgrade
+
+pkg install python python2 python3
+
+pip install requests mechanize
+
+pip2 install requests mechanize
+
+pkg install git
+
+pkg install ruby
+
+pkg install curl
+
+gem install lolcat
+
+pip2 install php
+
+pkg install figlet
+
+pkg install wget
+
+pkg install nano
+
+pkg install bash
+
+pkg install cowsay
+
+apt update && apt upgrade -y
+
+pkg install bash
+
+pkg install pv -y
+
+pkg install wget -y
+
+pkg install git -y
+fi
+
+## Menu2
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/Oh-My-Zsh
+cd Oh-My--Zsh
+git pull
+sh oh-my-zsh
+fi
+
+## MENU3
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/MTK
+cd MTK
+git pull
+sh mtk.sh
+fi
+
+## MENU4
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/dmbf
+cd dmbf
+git pull
+python dmbf.py
+fi
+
+
+## MENU5
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/KangSpam
+cd KangSpam
+git pull
+make termux
+bash Main.sh
+fi
+
+## MENU6
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/terkey
+cd terkey
+git pull
+python terkey.py
+fi
+
+## MENU7
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/spamcall
+cd spamcall
+git pull
+sh spam-call.sh
+fi
+
+## MENU8
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/Xenzi-XN1/Tools-Spam
+cd Tools-Spam
+git pull
+python Menu.py
+fi
+
+## MENU0
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya"
+sleep 5
+echo "\033[32;1mMR-Xyaa"
+exit
 fi
