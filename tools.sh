4fdbf7024ba11171732b927795ef6d69b5418dc3
IBNU UMAR SYAID <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-02 17:31:04 +0700

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,31 @@
+#!/system/xbin/bash/sh/python
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+
+
+cd module
+clear
+python loading.py
+
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+
+
+read -p "MR-Xyaa Pilih No:" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet -f slant "MR-Xyaa
+fi
