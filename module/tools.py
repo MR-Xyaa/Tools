9547b5924104acab95de0780e323584db49cf89c
Inu Ganteng <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-03 10:30:23 +0700

    Update tools.py

--- module/tools.py
+++ module/tools.py
@@ -13,7 +13,7 @@ import random
 import sys
 import time
 import os
-def mengetik(s):
+def inu_ganteng_banget(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -21,19 +21,19 @@ def mengetik(s):
 
 
 user_reply = input("Namamu Bang? \n")
-mengetik('\x1b[1;92m[+]===============[ MENU ]===============[+]')
-mengetik('\x1b[1;91m[1]___Install Bahan [Install Dulu Biar Work]')
-mengetik('\x1b[1;93m[2]___Tampilan Termux Oh-My-Zsh [Tampilan Termux]')
-mengetik('\x1b[1;94m[3]___MTK')
-mengetik('\x1b[1;95m[4]___DMBF [Dark Multi Brute Force]')
-mengetik('\x1b[1;96m[5]___KangSpam [Spam')
-mengetik('\x1b[1;97m[6]___Terkey')
-mengetik('\x1b[1;91m[7]___Spamcall')
-mengetik('\x1b[1;93m[8]___Spam By Bang Xenzi')
-mengetik('\x1b[1;94m[9]___DB-BOM Spam By Bang Xenzi')
-mengetik('\x1b[1;94m[10]___Spamer')
-mengetik('\x1b[1;95m[00]___EXIT TO MENU')
-mengetik('\x1b[1;92m[+]===============[ MENU ]===============[+]')
+inu_ganteng_banget('\x1b[1;92m[+]===============[ MENU ]===============[+]')
+inu_ganteng_banget('\x1b[1;91m[1]____Install Bahan')
+inu_ganteng_banget('\x1b[1;93m[2]____Tampilan Termux Oh-My-Zsh')
+inu_ganteng_banget('\x1b[1;94m[3]____Matematika')
+inu_ganteng_banget('\x1b[1;95m[4]____Dark Multi Brute Force')
+inu_ganteng_banget('\x1b[1;96m[5]____KangSpam')
+inu_ganteng_banget('\x1b[1;97m[6]____Terkey')
+inu_ganteng_banget('\x1b[1;91m[7]____Spamcall')
+inu_ganteng_banget('\x1b[1;93m[8]____Spam By Bang Xenzi')
+inu_ganteng_banget('\x1b[1;94m[9]____DB-BOM Spam By Bang Xenzi')
+inu_ganteng_banget('\x1b[1;94m[10]___Spamer')
+inu_ganteng_banget('\x1b[1;95m[00]___EXIT TO MENU')
+inu_ganteng_banget('\x1b[1;92m[+]===============[ MENU ]===============[+]')
 print("Bang", user_reply)
 mengetik('Pilih Menunya Bang')
 
